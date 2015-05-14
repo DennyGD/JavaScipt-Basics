69beb83021afc2e080c022260a050222fcc73528
DennyGD <denny.dimitrova@abv.bg>
2015-05-14 18:11:40 +0300

    Data types and variables

---
+++ DataTypesAndVariables/DataTypesAndVariables/dataTypesAndVariables.js
@@ -0,0 +1,24 @@
+﻿var someString = "some string";
+var integer = 100;
+var floatingPoint = 100.1;
+var isTrue = true;
+var arrayAllInOne = [someString, integer, floatingPoint, isTrue];
+
+console.log(arrayAllInOne);
+
+var joeysWords = "'How you doin'?', Joey said.";
+console.log(joeysWords);
+
+joeysWords = '\'How you doin\'?\', Joey said.';
+console.log("Joey's words again (using slashes): ", joeysWords);
+console.log("typeof joeysWords:", typeof (joeysWords));
+
+console.log(`typeof ${someString}:`, typeof (someString));
+console.log(`typeof ${integer}:`, typeof (integer));
+console.log(`typeof ${floatingPoint}:`, typeof (floatingPoint));
+console.log(`typeof ${isTrue}:`, typeof (isTrue));
+
+var undefinedVariable;
+var nullVariable = null;
+console.log("This is an undefined variable:", undefinedVariable);
+console.log("This is nullVariable:", nullVariable);
